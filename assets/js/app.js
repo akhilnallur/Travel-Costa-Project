c4acdb547bf416b0f83eb705e7748a44aedb0e1c
akhilnallur <akhilnallur000@gmail.com>
2022-06-10 11:25:12 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+let slideNav = document.querySelector('.slide-nav');
+let button = document.querySelector('.btn');
+
+
+
+function showNav (){
+    slideNav.classList.toggle('show');
+}
+
+// button.addEventListener('click', () => {
+//     slideNav.classList.toggle('show');
+//     let show = document.querySelector('.show');
+//     show.style.transform = 'translateX(-130px)';
+//     show.style.transition = '1s ease'
+// })
